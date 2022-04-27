0a8c32a1dba49cfd969a3c3d5c700d8a99d3ead4
paras <parasverma2531@gmail.com>
2022-04-28 01:10:35 +0530

    git modified

--- LinkedList/Node.java
+++ LinkedList/Node.java
@@ -2,10 +2,11 @@ package LinkedList;
 
 public class Node {
     public Node next;
-    public int data;
+    public int data;public static int length=0;
     public Node(int data)
     {
         this.data=data;
         this.next=null;
+        length++;
     }
 }
