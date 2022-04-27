cd58d449d1846afc3ee2aaaf1835cd5444a9fbad
paras <parasverma2531@gmail.com>
2022-04-28 01:14:27 +0530

    Singly main file

---
+++ SinglyAllFunctions_Main.java
@@ -0,0 +1,55 @@
+import LinkedList.SinglyAllFunctions;
+import java.util.Scanner;
+public class SinglyAllFunctions_Main {
+    public static void main(String[] args) {
+        int choice, data, result, position;
+        SinglyAllFunctions sl = new SinglyAllFunctions();
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            System.out.println("'1' for insertion At front\n'2' for insertion At End\n'3' for insertion At Pos\n'4' for deletion from Front\n'5' for deletion from end\n'6' for deletion from pos\n'7' for display\n'8' to exit");
+            choice = in.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.print("enter the data: ");
+                    data = in.nextInt();
+                    sl.addAtFront(data);
+                    break;
+                case 2:
+                    System.out.print("enter the data: ");
+                    data = in.nextInt();
+                    sl.addAtEnd(data);
+                    break;
+                case 3:
+                    System.out.print("enter the data and position: ");
+                    data = in.nextInt();
+                    position = in.nextInt();
+                    sl.addAtPos(data,position);
+                    break;
+                case 4:
+                    result = sl.deleteFromBeg();
+                    System.out.println(result == -1 ? "list is empty" : "the deleted data is :" + result);
+                    break;
+                case 5:
+                    result= sl.deleteFromEnd();
+                    System.out.println(result == -1 ? "list is empty" : "the deleted data is :" + result);
+                    break;
+                case 6:
+                    System.out.print("enter the position from where u want to delete data: ");
+                    position=in.nextInt();
+                    result=sl.deleteFromPos(position);
+                    System.out.println(result == -1 ? "list is empty" : "the deleted data is :" + result);
+                    break;
+                case 7:
+                    sl.display();
+                    break;
+                case  8:
+                    System.exit(0);
+                default:
+                    System.out.println("enter valid input");
+
+
+            }
+
+        }
+    }
+}
