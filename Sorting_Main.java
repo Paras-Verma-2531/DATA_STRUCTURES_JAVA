c6caf2236ea16fb6bbff886818491c5fe983d352
paras <parasverma2531@gmail.com>
2022-04-29 03:41:34 +0530

    bubble main

---
+++ Sorting_Main.java
@@ -0,0 +1,32 @@
+import Sorting.*;
+import java.util.Scanner;
+public class Sorting_Main {
+    public void display(int array[],int size)
+    {
+        System.out.println("sorted array is: ");
+        for(int i:array)
+        {
+            System.out.println(i);
+        }
+    }
+    public void inputElements(int array[],int size)
+    {Scanner in=new Scanner(System.in);
+        for(int i=0;i<size;i++)
+        {
+            System.out.print("enter the "+(i+1) +" element: ");
+            array[i]=in.nextInt();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int size;
+        System.out.print("enter the size of array: ");
+        size=in.nextInt();
+        int array[]=new int[size];
+        Sorting_Main sort=new Sorting_Main();
+        sort.inputElements(array,size);
+        bubble_Sort bl=new bubble_Sort();
+        bl.bubbleSort(array,size);
+        sort.display(array,size);
+    }
+}
