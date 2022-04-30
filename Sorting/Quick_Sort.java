8a9a78dd65c83d6e7541543bdd6c2667371283d1
paras <parasverma2531@gmail.com>
2022-04-30 21:46:32 +0530

    Quick Sort

---
+++ Sorting/Quick_Sort.java
@@ -0,0 +1,36 @@
+package Sorting;
+
+public class Quick_Sort {
+    public int partition(int array[],int initial,int end)
+    {
+        int start=initial;int end_p=end;int pivot=array[initial];
+        System.out.println("start pos at beg is "+start);
+        //System.out.println("end pos at beg is "+end_p);
+        while(start<end_p) {
+            while (array[start] <= pivot) {
+                start++;
+                System.out.println("start is now at "+start);
+            }
+            //System.out.println("start is now at "+start);
+            while (array[end_p] > pivot) {
+                end_p--;
+            }//System.out.println("end is now at "+end_p);
+            if (start < end_p) {
+                int temp = array[start];
+                array[start] = array[end_p];
+                array[end_p] = temp;
+            }
+        }
+        int temp = array[initial];
+             array[initial]=array[end_p];
+             array[end_p]=temp;
+             return end_p;
+    }
+    public void QuickSort(int array[],int initial,int end) {
+        if (initial < end) {
+            int pivot = partition(array, initial, end);
+            QuickSort(array, initial, pivot - 1);
+            QuickSort(array, pivot + 1, end);
+        }
+    }
+}
