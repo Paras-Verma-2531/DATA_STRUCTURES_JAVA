60a183a508bda5abdd1ce2d4823157aca7bf1af7
paras <parasverma2531@gmail.com>
2022-06-11 12:49:23 +0530

    Sorting/BubbleSort

--- DSA_PRAC/SORTING/BubbleSort.java
+++ DSA_PRAC/SORTING/BubbleSort.java
@@ -3,6 +3,13 @@ package DSA_PRAC.SORTING;
 import java.util.Scanner;
 
 public class BubbleSort {
+    /*
+    In BubbleSort, we compare two adjacent elements from the leftside and oif leftmost is greater than other element, we will swap these two elements.
+    in this manner, we will bubble out the biggest element to the rightmost side of array.
+    ===================
+    Time complexity of BubbleSort is :
+    O(n^2)--> Big O of n^2
+     */
     public void bubbleSort(int[] array)
     {int temp=0;
         for(int i=0;i<9;i++)
@@ -23,7 +30,7 @@ public class BubbleSort {
         int[] arr= new int[10];
         for(int i=0;i<10;i++)
         {
-            System.out.println("enter the "+(i+1)+" number");
+            System.out.print("enter the "+(i+1)+" number: ");
             arr[i]=in.nextInt();
         }
         new BubbleSort().bubbleSort(arr);
