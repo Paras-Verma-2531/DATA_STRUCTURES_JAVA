9ad7fcb6a0061f364379f6a85e0681483281d7b8
paras <parasverma2531@gmail.com>
2022-06-14 09:24:07 +0530

    reverseOfArray method

---
+++ DSA_PRAC/reverseOfArray.java
@@ -0,0 +1,26 @@
+package DSA_PRAC;
+public class reverseOfArray {
+    public static void swap(int[] array,int i , int j)
+    {
+        int temp=array[i];
+        array[i]=array[j];
+        array[j]=temp;
+    }
+    public static void reverse(int[] array,int start,int end)
+    {
+        while(start<end)
+        {
+            swap(array,start,end);
+            start++;end--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] array={1,2,3,4,5,6,7,8,9,10};
+        reverseOfArray.reverse(array,0,9);
+        for(int i: array)
+        {
+            System.out.println(i);
+        }
+    }
+
+}
