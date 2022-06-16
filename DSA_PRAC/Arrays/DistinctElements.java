58b4812883c1f073832af0a11c36c85ebd0ddedb
paras <parasverma2531@gmail.com>
2022-06-16 18:21:02 +0530

    DistinctElement

--- DSA_PRAC/Arrays/DistinctElements.java
+++ DSA_PRAC/Arrays/DistinctElements.java
@@ -1,9 +1,12 @@
 package DSA_PRAC.Arrays;
 
+import java.util.HashSet;
+import java.util.Set;
 public class DistinctElements {
-    public int countDistinctElements(int[] array,int length) {
+    public int countDistinctElements(int[] array,int length)//O(n^2)
+    {
         int res = 0, count = 0;
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i <length; i++) {
             int j = 0;
             while (j < i) {
                 if (array[j] == array[i]) {
@@ -18,9 +21,18 @@ public class DistinctElements {
             return res+1;// else it misses out the first element.
         return res;
     }
+    // 2nd approach through set
+    public int countDistinct(int[] array)
+    {
+        Set<Integer> st= new HashSet<>();
+        for(int i:array)
+            st.add(i);// add all element
+        return st.size();// returns the size[ no duplicate]
+    }
 
     public static void main(String[] args) {
         int[] array={1,2,3,4,5,1,2,3};
         System.out.println(new DistinctElements().countDistinctElements(array,array.length));
+        System.out.println(new DistinctElements().countDistinct(array));
     }
 }
