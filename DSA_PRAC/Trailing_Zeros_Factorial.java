0034dd1ec50f35e6510114196f21fe55859d2cf0
paras <parasverma2531@gmail.com>
2022-06-26 10:30:11 +0530

    find trailing zeros in factorial

---
+++ DSA_PRAC/Trailing_Zeros_Factorial.java
@@ -0,0 +1,19 @@
+package DSA_PRAC;
+
+public class Trailing_Zeros_Factorial {
+    public int findTrailingZeros(int num)
+    {
+        int res=0;
+        for(int i=5;i<=num;i=i*5)
+        {
+            res=res+num/i;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        // since every number whose factorial ends with 0s contains 2 and 5 where 5 are less  than 2. Therefore, used the above logic.
+        // no need to calculate fact. since they will go beyond the range of long and int.
+        System.out.println(new Trailing_Zeros_Factorial().findTrailingZeros(5));
+    }
+}
