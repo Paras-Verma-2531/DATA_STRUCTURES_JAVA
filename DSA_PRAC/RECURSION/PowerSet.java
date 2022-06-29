45a637392ec876ee2f04af21f8d9d7cd34da1639
paras <parasverma2531@gmail.com>
2022-06-29 11:03:23 +0530

    power Set of String

---
+++ DSA_PRAC/RECURSION/PowerSet.java
@@ -0,0 +1,27 @@
+package DSA_PRAC.RECURSION;
+
+public class PowerSet {
+    /*
+    Recursive approach used to find power set of a string
+    if i==length
+    [print curr]
+    we have 2 choices i,e either to include character or not
+    therefore,two recursive calls
+     powerSet(st,i+1,curr+st.charAt(i));
+      powerSet(st,i+1,curr);
+     */
+    public void powerSet(String st,int i,String curr)
+    {
+        if(i==st.length())
+        {
+            System.out.println(curr);
+            return;
+        }
+        powerSet(st,i+1,curr+st.charAt(i));
+        powerSet(st,i+1,curr);
+    }
+
+    public static void main(String[] args) {
+       new PowerSet().powerSet("abc",0," ");
+    }
+}
