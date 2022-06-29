fabbc43db10205794880dafb0673159420945e34
paras <parasverma2531@gmail.com>
2022-06-29 10:45:44 +0530

    Palindrome String/recursion

---
+++ DSA_PRAC/RECURSION/Palindrome_String.java
@@ -0,0 +1,17 @@
+package DSA_PRAC.RECURSION;
+
+public class Palindrome_String {
+    public boolean isPalindrome(String s,int left,int right)
+    {
+        if(left>=right)// if only one character is present
+            return true;
+        if(s.charAt(left)!=s.charAt(right))// if character does not match
+            return false;
+        return isPalindrome(s,left+1,right-1);// recursive call
+
+    }
+    public static void main(String[] args) {
+        String name="naman";
+        System.out.println(new Palindrome_String().isPalindrome(name,0,name.length()-1));
+    }
+}
