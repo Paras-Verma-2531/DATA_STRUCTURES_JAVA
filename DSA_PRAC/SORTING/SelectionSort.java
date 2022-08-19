e25f636efd4895eca9619df41506a637f1c96912
paras <parasverma2531@gmail.com>
2022-08-19 15:10:47 +0530

    Selection Sort

--- DSA_PRAC/SORTING/SelectionSort.java
+++ DSA_PRAC/SORTING/SelectionSort.java
@@ -1,17 +1,17 @@
 package DSA_PRAC.SORTING;
 import java.util.Scanner;
 public class SelectionSort {
-    public void selectionSort(int[] array)
+    public void selectionSort(int[] array,int size)
     { int min=0;
-        for(int i=0;i<array.length-1;i++)
+        for(int i=0;i<size-1;i++)
         {
             min=i;
-            for(int j=i+1;j<array.length;j++)
+            for(int j=i+1;j<size;j++)
             {
-                if(array[j]<array[min])
-                    min=j;
+                if(array[j]<array[min])//search for the minimum element
+                    min=j;//make min as index of minimum element
             }
-            int temp=array[i];
+            int temp=array[i];//swap arr[min] with arr[i]
             array[i]=array[min];
             array[min]=temp;
         }
@@ -24,10 +24,8 @@ public class SelectionSort {
             System.out.print("enter the "+(i+1)+ " number :");
             arr[i]=in.nextInt();
         }
-        new SelectionSort().selectionSort(arr);
+        new SelectionSort().selectionSort(arr,10);
         for(int i:arr)
-        {
             System.out.println(i);
-        }
     }
 }
