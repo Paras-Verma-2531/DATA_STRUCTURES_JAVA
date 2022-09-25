c534a20c17d91585163326f64b33ade0e7cad930
paras <parasverma2531@gmail.com>
2022-09-25 17:48:12 +0530

    Pattern

--- DSA_PRAC/PATTERN/Pattern_1.java
+++ DSA_PRAC/PATTERN/Pattern_1.java
@@ -4,7 +4,15 @@ public class Pattern_1 {
         pattern1(4);
         System.out.println("\tPattern 2");
         patter2(5);
+        System.out.println("\tPattern 3");
+        pattern3(5);
     }
+    /*
+*
+**
+***
+****
+*/
     static void pattern1(int n)
     {// outer loop will run the no of times=no of rows
         for(int i=1;i<=n;i++)
@@ -13,11 +21,33 @@ public class Pattern_1 {
             System.out.println();
         }
     }
+    /*
+    *****
+    *****
+    *****
+    *****
+    *****
+     */
     static void patter2(int n)
     {
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=n;j++) System.out.print("*");
+            System.out.println();
+        }
+    }
+    /*
+    *****
+    ****
+    ***
+    **
+    *
+     */
+    static void pattern3(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=i;j<=n;j++) System.out.print("*");
             System.out.println();
         }
     }
