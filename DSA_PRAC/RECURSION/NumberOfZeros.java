95e5721aaff6b16ccdfd7af3e4c5efe199bfedb6
paras <parasverma2531@gmail.com>
2022-10-23 23:22:10 +0530

    Count Number of Zeros

---
+++ DSA_PRAC/RECURSION/NumberOfZeros.java
@@ -0,0 +1,14 @@
+package DSA_PRAC.RECURSION;
+public class NumberOfZeros {
+    public static void main(String[] args) {
+        System.out.println(countNumberOfZeros(302));
+    }
+    static int countNumberOfZeros(int n)
+    {
+        return count(n,0);//count is an helper function which actually returns the result
+    }
+    private static int count(int n, int i) {
+        if(n==0)return i;// no itself is zero
+        return n%10==0?count(n/10,i+1):count(n/10,i);
+    }
+}
