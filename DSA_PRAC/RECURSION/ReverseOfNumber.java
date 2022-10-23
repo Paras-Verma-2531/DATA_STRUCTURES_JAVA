58aab014ac74007a724fc3fdd454be12ac5db72e
paras <parasverma2531@gmail.com>
2022-10-23 23:05:19 +0530

    Reverse A Number

---
+++ DSA_PRAC/RECURSION/ReverseOfNumber.java
@@ -0,0 +1,16 @@
+package DSA_PRAC.RECURSION;
+public class ReverseOfNumber {
+    public static void main(String[] args) {
+        System.out.println(reverse(1234));
+    }
+    static int reverse(int n)
+    {
+        int len=(int)Math.log10(n)+1;
+        return helper(n,len);
+    }
+    private static int helper(int n, int len) {
+        if(n%10==n)return n;
+        //logic is: make 10 to the power of length-1 * extracted digit and add it with for n/10;
+        return (n%10)*(int)Math.pow(10,len-1)+helper(n/10,len-1);
+    }
+}
