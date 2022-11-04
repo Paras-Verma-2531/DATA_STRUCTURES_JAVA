bff0ef2960254141c2a2b3b249f790d6323b08dc
paras <parasverma2531@gmail.com>
2022-11-05 04:09:34 +0530

    SubSequence

---
+++ DSA_PRAC/RECURSION/SubSequence.java
@@ -0,0 +1,15 @@
+package DSA_PRAC.RECURSION;
+public class SubSequence {
+    public void subSeq(String string)
+    {
+        helper("",string);
+    }
+    private void helper(String s, String string) {
+        if(string.isEmpty()) {System.out.println(s);return;}
+        helper(s+string.charAt(0),string.substring(1));//include that character
+        helper(s,string.substring(1));//ignore that character
+    }
+    public static void main(String[] args) {
+        new SubSequence().subSeq("abc");
+    }
+}
