9ac333107b42e37845d1df8d9f23f5632308825f
paras verma <parasverma2531@gmail.com>
2022-12-14 04:21:11 +0530

    Update Quick_Sort.java

--- Sorting/Quick_Sort.java
+++ Sorting/Quick_Sort.java
@@ -1,11 +1,23 @@
 package Sorting;
 public class Quick_Sort {
-    public int partition(int[]array,int initial,int end)
+//     public int partition(int[]array,int initial,int end)
+//     {
+//         int start=initial;int end_p=end;int pivot=array[initial];
+//         while(start<end_p) {
+//             while ( start<=end_p &&array[start] <= pivot)start++;
+//             while (array[end_p] > pivot)end_p--;
+//             if (start < end_p) {
+//                 int temp = array[start];
+//                 array[start] = array[end_p];
+//                 array[end_p] = temp;
+//             }
+//         }
+     public int partition(int[]array,int initial,int end)
     {
-        int start=initial;int end_p=end;int pivot=array[initial];
+        int start=initial;int end_p=end;int pivot=array[end];
         while(start<end_p) {
-            while ( start<=end_p &&array[start] <= pivot)start++;
-            while (array[end_p] > pivot)end_p--;
+            while ( start<=end_p &&array[start] <pivot)start++;
+            while (end_p>start&&array[end_p]>=pivot)end_p--;
             if (start < end_p) {
                 int temp = array[start];
                 array[start] = array[end_p];
