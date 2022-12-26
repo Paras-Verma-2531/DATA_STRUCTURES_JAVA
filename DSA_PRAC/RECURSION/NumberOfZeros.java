6e9c2c9dce2ad52e7337068185f5cb891b94c646
paras <parasverma2531@gmail.com>
2022-12-26 06:59:55 +0530

    CountNumberOfZeros

--- DSA_PRAC/RECURSION/NumberOfZeros.java
+++ DSA_PRAC/RECURSION/NumberOfZeros.java
@@ -3,12 +3,18 @@ public class NumberOfZeros {
     public static void main(String[] args) {
         System.out.println(countNumberOfZeros(302));
     }
-    static int countNumberOfZeros(int n)
+//    static int countNumberOfZeros(int n)
+//    {
+//        return count(n,0);//count is an helper function which actually returns the result
+//    }
+//    private static int count(int n, int i) {
+//        if(n==0)return i;// no itself is zero
+//        return n%10==0?count(n/10,i+1):count(n/10,i);
+//    }
+      static int countNumberOfZeros(int n)
     {
-        return count(n,0);//count is an helper function which actually returns the result
-    }
-    private static int count(int n, int i) {
-        if(n==0)return i;// no itself is zero
-        return n%10==0?count(n/10,i+1):count(n/10,i);
+        if(n==0)return 0;
+        if(n%10==0)return 1+ countNumberOfZeros(n/10);
+        return countNumberOfZeros(n/10);
     }
 }
