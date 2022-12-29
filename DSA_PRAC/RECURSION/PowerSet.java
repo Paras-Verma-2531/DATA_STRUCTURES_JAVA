2354e803e8c628d6fb8da8c28d965da1cfec2a75
paras <parasverma2531@gmail.com>
2022-12-29 15:56:23 +0530

    PowerSet-using List

--- DSA_PRAC/RECURSION/PowerSet.java
+++ DSA_PRAC/RECURSION/PowerSet.java
@@ -1,4 +1,7 @@
 package DSA_PRAC.RECURSION;
+
+import java.util.ArrayList;
+
 public class PowerSet {
     /*
     Recursive approach used to find power set of a string
@@ -9,17 +12,33 @@ public class PowerSet {
      powerSet(st,i+1,curr+st.charAt(i));
       powerSet(st,i+1,curr);
      */
-    public void powerSet(String st,int i,String curr)
+//    public void powerSet(String st,int i,String curr)
+//    {
+//        if(i==st.length())
+//        {
+//            System.out.println(curr);
+//            return;
+//        }
+//        powerSet(st,i+1,curr+st.charAt(i));
+//        powerSet(st,i+1,curr);
+//    }
+    //using LinkedList::
+    public ArrayList<String> powerSet(String st,int i,String curr)
     {
         if(i==st.length())
         {
-            System.out.println(curr);
-            return;
+            ArrayList<String>list=new ArrayList <>();
+            list.add(curr);
+            return list;
         }
-        powerSet(st,i+1,curr+st.charAt(i));
-        powerSet(st,i+1,curr);
+        ArrayList<String> left=powerSet(st,i+1,curr+st.charAt(i));
+        ArrayList<String>right=powerSet(st,i+1,curr);
+        left.addAll((right));
+        return left;
     }
     public static void main(String[] args) {
-       new PowerSet().powerSet("abc",0," ");
+       ArrayList<String>list=new ArrayList <>();
+        list=new PowerSet().powerSet("abc",0," ");
+        System.out.println(list);
     }
 }
