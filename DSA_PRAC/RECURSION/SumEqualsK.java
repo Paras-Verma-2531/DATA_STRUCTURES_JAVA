da2d2d61f722f6c78421249f4be7fc544d03f89b
paras <parasverma2531@gmail.com>
2023-01-13 22:35:28 +0530

    return no of counts-sumEqualsK

--- DSA_PRAC/RECURSION/SumEqualsK.java
+++ DSA_PRAC/RECURSION/SumEqualsK.java
@@ -12,7 +12,7 @@ public class SumEqualsK {
 //        currsum-=arr[index];//subtract from curr_sum; --->not taking
 //        sumEqualsK(arr,list,index+1,sum,currsum);
 //    }
-    //if either the one sol exists return true;
+    /*if either the one sol exists return true;
     public static boolean sumEqualsK(int[]arr, ArrayList<Integer>list,int index, int sum, int currsum)
     {
         if(index==arr.length)
@@ -28,7 +28,25 @@ public class SumEqualsK {
         currsum-=arr[index];//subtract from curr_sum; --->not taking
         return sumEqualsK(arr, list, index + 1, sum, currsum);
     }
+    */
+    //return no of count's::
+    public static int sumEqualsK(int[]arr, ArrayList<Integer>list,int index, int sum, int currsum)
+    {
+        if(index==arr.length)
+        {if(currsum==sum){
+            //condition satisfied
+            System.out.println(list);return 1;
+        }return 0;
+        }
+        list.add(arr[index]);//add the element in the list --->taking
+        currsum+=arr[index];//add it to the currsum as well
+        int left=sumEqualsK(arr,list,index+1,sum,currsum);//pass with additional included sum::
+        list.remove(list.size()-1);//remove from the list
+        currsum-=arr[index];//subtract from curr_sum; --->not taking
+        int right=sumEqualsK(arr, list, index + 1, sum, currsum);
+        return left+right;
+    }
     public static void main(String[] args) {
-        SumEqualsK.sumEqualsK(new int[]{1,2,1},new ArrayList<Integer>(),0,2,0);
+        System.out.println(SumEqualsK.sumEqualsK(new int[]{1,2,1},new ArrayList<Integer>(),0,2,0));
     }
 }
