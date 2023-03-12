a5e0efd604aa55e81cb3f876315ade9c632aaca2
paras <parasverma2531@gmail.com>
2023-03-12 23:26:44 +0530

    Union using Optimal Solution

--- DSA_PRAC/Arrays/UnionOfTwoSortedArray.java
+++ DSA_PRAC/Arrays/UnionOfTwoSortedArray.java
@@ -1,10 +1,8 @@
 package DSA_PRAC.Arrays;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
+
 public class UnionOfTwoSortedArray {
-    //brute force Approach::
+    /*brute force Approach::
     public int[] union(int[]arr1,int[]arr2)
     {
         Set <Integer> set=new HashSet<>();
@@ -18,6 +16,48 @@ public class UnionOfTwoSortedArray {
         while(it.hasNext())
             new_arr[j++]=it.next();
         return new_arr;
+    }*/
+    public Object[] union(int[]arr1,int[]arr2)
+    {
+        int i=0,j=0,k=-1;
+        ArrayList<Integer>list=new ArrayList<>();
+        while(i<arr1.length&&j<arr2.length)
+        {
+            if(arr1[i]<=arr2[j])
+            {
+                if(k==-1||list.get(k)!=arr1[i])
+                {
+                    list.add(arr1[i]);k++;
+                }i++;
+            }
+            else {
+                if(k==-1||list.get(k)!=arr2[j])
+                {
+                    list.add(arr2[j]);k++;
+                }j++;
+            }
+        }
+        if(i<arr1.length)
+        {
+            while(i<arr1.length)
+            {
+                if(k==-1||list.get(k)!=arr1[i])
+                {
+                    list.add(arr1[i]);k++;
+                }i++;
+            }
+        }
+        if(j<arr2.length)
+        {
+            while(j<arr2.length)
+            {
+                if(k==-1||list.get(k)!=arr2[j])
+                {
+                    list.add(arr2[j]);k++;
+                }j++;
+            }
+
+        }return list.toArray();
     }
     public static void main(String[] args) {
         System.out.println(Arrays.toString(new UnionOfTwoSortedArray().union(new int[]{1,2,3,4,5},new int[]{2,3,4,5,6,7,7})));
