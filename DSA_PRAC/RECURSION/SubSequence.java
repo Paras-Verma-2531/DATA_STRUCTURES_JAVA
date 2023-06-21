dac21565baf1638b2b313f87c7bde5835b73195c
paras <parasverma2531@gmail.com>
2023-06-22 01:12:51 +0530

    Subsequence revisited

--- DSA_PRAC/RECURSION/SubSequence.java
+++ DSA_PRAC/RECURSION/SubSequence.java
@@ -1,4 +1,7 @@
 package DSA_PRAC.RECURSION;
+
+import java.util.ArrayList;
+
 public class SubSequence {
     public void subSeq(String string)
     {
@@ -9,7 +12,21 @@ public class SubSequence {
         helper(s+string.charAt(0),string.substring(1));//include that character
         helper(s,string.substring(1));//ignore that character
     }
+//    for the integer using List
+    public void printSubseq(int index, ArrayList<Integer>list,int[]arr)
+    {
+        if(index==arr.length)
+        {
+            System.out.println(list.toString());
+            return;
+        }
+        list.add(arr[index]);
+        printSubseq(index+1,list,arr);
+        list.remove(list.size()-1f);
+        printSubseq(index+1,list,arr);
+    }
     public static void main(String[] args) {
-        new SubSequence().subSeq("abc");
+        new SubSequence().printSubseq(0,new ArrayList<Integer>(),new int[]{3,1,2});
+//        new SubSequence().subSeq("abc");
     }
 }
